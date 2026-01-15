ce254d1dd59dd6324e900642f36ce3a36b0131d6
JuanP4bl0-c <jpgbamil@gmail.com>
2026-01-15 01:25:37 -0300

    Adição aos testes de Edição - Aviso da discrepancia da tabela de emails e telefone

--- database/Testes_opcional.sql
+++ database/Testes_opcional.sql
@@ -1,6 +1,11 @@
-select * from profissional;
-delete * from consultas;
+select * from atendimento;
+select * from consulta;
 select * from pessoa;
-select profissional;
+select * from agencia;
+select * from prescricao;
+select * from profissional;
 
 TRUNCATE TABLE pessoa cascade;
+TRUNCATE TABLE pessoa RESTART IDENTITY CASCADE;
+TRUNCATE TABLE profissional RESTART IDENTITY CASCADE;
+
